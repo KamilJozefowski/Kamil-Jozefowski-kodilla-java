a74e691d3ef34cc7277bbc68d54c117b8fd23880
Kamil Józefowski <k.m.jozefowski@gmail.com>
2020-08-25 16:14:51 +0200

    8.4 Task: knowledge of exceptions

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FindFilght.java
@@ -0,0 +1,28 @@
+package com.kodilla.exception.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FindFilght {
+
+    public boolean FindFilght(Flight flight) throws RouteNotFoundException {
+        Map <String, Boolean> airports = new HashMap<>();
+
+        airports.put("Warszawa", true);
+        airports.put("Berlin", true);
+        airports.put("Londyn", true);
+        airports.put("Moskwa", false);
+
+        if ( airports.containsKey(flight.getArrivalAirport())
+                && airports.containsKey(flight.getDepartureAirport())
+                && airports.get(flight.getArrivalAirport())
+                && airports.get(flight.getDepartureAirport())) {
+            return true;
+        } else {
+            throw new RouteNotFoundException("Arrival airport doesn't exits in flight connection map");
+        }
+
+
+
+    }
+}
---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/Main.java
@@ -0,0 +1,33 @@
+package com.kodilla.exception.test;
+
+public class Main {
+    public static void main(String[] args) {
+        FindFilght findFilght = new FindFilght();
+
+        try {
+            if (findFilght.FindFilght(new Flight("Warszawa", "Londyn"))) {
+                System.out.println("We have connections to these airports");
+            }
+        } catch (RouteNotFoundException e) {
+            System.out.println("Sorry, flight not exists. Arrival or departure airport not exist in our connection map");
+        }
+
+        try {
+            if (findFilght.FindFilght(new Flight("Madryt", "Londyn"))) {
+                System.out.println("We have connections to these airports");
+            }
+        } catch (RouteNotFoundException e) {
+            System.out.println("Sorry, flight not exists. Arrival or departure airport not exist in our connection map");
+        }
+
+        try {
+            if (findFilght.FindFilght(new Flight("Warszawa", "Moskwa"))) {
+                System.out.println("We have connections to these airports");
+            }
+        } catch (RouteNotFoundException e) {
+            System.out.println("Sorry, flight not exists. Arrival or departure airport not exist in our connection map");
+        }
+
+
+    }
+}
