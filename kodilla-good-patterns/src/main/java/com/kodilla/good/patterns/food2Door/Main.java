096a1f6c6d42f269f60b5de89a2e11814da25e77
Kamil Józefowski <k.m.jozefowski@gmail.com>
2020-10-01 17:36:52 +0200

    13.4 Zadanie: wyzwanie - Food2Door

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/food2Door/Main.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/food2Door/Main.java
@@ -5,17 +5,35 @@ import java.time.LocalDateTime;
 public class Main {
 
     public static void main(String[] args) {
-
-        LocalAddress localAddress = new LocalAddress("London", "Clone", 18);
-        ProductsSold productsSold = new ProductsSold("chair", 400);
-        Supplier supplier = new Supplier("Korex", localAddress, productsSold);
-        Customer customer = new Customer("John", "Kolonko", localAddress);
+        LocalAddress localAddressCustomer = new LocalAddress("London", "George st", 46);
+        Customer customer = new Customer("John", "Kolonko", localAddressCustomer);
         LocalDateTime time = LocalDateTime.now();
+
+        //ExtraFoodShop company
+        LocalAddress localAddress = new LocalAddress("London", "Clone st", 18);
+        ProductsSold productsSold = new ProductsSold("corn", 400);
+        Supplier supplier = new Supplier("ExtraFoodShop", localAddress, productsSold);
+
+        //HealthyShop company
+        LocalAddress localAddress1 = new LocalAddress("London", "the Mall", 6);
+        ProductsSold productsSold1 = new ProductsSold("bio apple", 1200);
+        Supplier supplier1 = new Supplier("HealthyShop", localAddress1, productsSold1);
+
+        //GlutenFreeShop company
+        LocalAddress localAddress2 = new LocalAddress("London", "Ogle st", 112);
+        ProductsSold productsSold2 = new ProductsSold("cheese", 57);
+        Supplier supplier2 = new Supplier("GlutenFreeShop", localAddress2, productsSold2);
+
         Order order = new Order(supplier, customer,time);
+        Order order1 = new Order(supplier1, customer,time);
+        Order order2 = new Order(supplier2, customer,time);
+
 
         SellingProcess sellingProcess = new SellingProcess(order,new InformationService(customer),new SellingService());
 
         sellingProcess.process(order);
+        sellingProcess.process(order1);
+        sellingProcess.process(order2);
 
     }
 }
