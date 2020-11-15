4c1ed0e85abfbd01bd75633cad9c2f1ae751979c
Kamil Józefowski <k.m.jozefowski@gmail.com>
2020-11-15 21:58:48 +0100

    17.3. Zadanie: nagłówki faktur oraz ich pozycje

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/invoice/dao/InvoiceDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/invoice/dao/InvoiceDaoTestSuite.java
@@ -6,11 +6,13 @@ import com.kodilla.hibernate.invoice.Product;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
+@Transactional
 @SpringBootTest
 public class InvoiceDaoTestSuite {
 
